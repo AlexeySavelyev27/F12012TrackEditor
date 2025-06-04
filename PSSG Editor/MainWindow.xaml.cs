ad9a887993c392fffa0ec1bfdaacdf9984f1a0e9
AlexeySavelyev27 <promixnaugust@gmail.com>
2025-06-04 12:47:46 +0300

    Fix DataGrid editing focus issues

--- PSSG Editor/MainWindow.xaml.cs
+++ PSSG Editor/MainWindow.xaml.cs
@@ -557,6 +557,7 @@ namespace PSSGEditor
 
                     // Разрешаем клики ставить курсор без выделения
                     tb.PreviewMouseLeftButtonDown += ValueTextBox_PreviewMouseLeftButtonDown;
+                    tb.MouseLeftButtonDown += ValueTextBox_MouseLeftButtonDown;
                 }
             }
         }
@@ -656,6 +657,12 @@ namespace PSSGEditor
                     charIndex = tb.Text.Length;
                 tb.CaretIndex = charIndex;
             }
+        }
+
+        private void ValueTextBox_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Предотвращаем передачу кликов DataGrid, чтобы повторные клики не вызывали выделение ячейки
+            e.Handled = true;
         }
 
         // Пересчитать ширину первого столбца (Attribute) по содержимому
