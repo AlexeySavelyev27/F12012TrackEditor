b118478189da7f0a625ca3763fe72474b0f2351e
AlexeySavelyev27 <promixnaugust@gmail.com>
2025-06-05 16:17:50 +0300

    fix editing focus in value field

--- PSSG Editor/MainWindow.xaml.cs
+++ PSSG Editor/MainWindow.xaml.cs
@@ -370,6 +370,15 @@ namespace PSSGEditor
 
             if (depObj is DataGridCell cell)
             {
+                // When the clicked cell is already in edit mode, do not let the
+                // DataGrid process the click. This prevents the cell from being
+                // re-selected or losing the current caret position while editing.
+                if (cell.IsEditing)
+                {
+                    e.Handled = true;
+                    return;
+                }
+
                 // If click on the "Attribute" column, immediately jump to "Value" cell in the same row
                 if (cell.Column.DisplayIndex == 0)
                 {
